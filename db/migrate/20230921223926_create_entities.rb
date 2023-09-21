2f305416d2545038e589bd4f5ceae07b05b7a6e6
cherelemma <chere.lemma@aastu.edu.et>
2023-09-22 01:46:59 +0300

    run migration and create a schema

--- db/migrate/20230921223926_create_entities.rb
+++ db/migrate/20230921223926_create_entities.rb
@@ -3,7 +3,7 @@ class CreateEntities < ActiveRecord::Migration[7.0]
     create_table :entities do |t|
       t.string :name
       t.decimal :amount
-      t.references :user, null: false, foreign_key: true { to_table: :users}
+      t.references :user, null: false, foreign_key: { to_table: :users}
       t.references :category, null: false, foreign_key: true
 
       t.timestamps
