a291f23f35f1d97ab7b2913ff191bdf7d033b6a6
cherelemma <chere.lemma@aastu.edu.et>
2023-09-22 22:57:18 +0300

    add a user model unit test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,70 @@
+# Test for User model
+# spec/modules/user_spec.rb
+#
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:user) { FactoryBot.build(:user) }
+  
+  it 'is valid with valid attributes' do
+    expect(user).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    user.name = nil
+    expect(user).to_not be_valid
+  end
+
+  it 'is not valid with a name longer than 60 characters' do
+    user.name = ('a' * 61)
+    expect(user).to_not be_valid
+  end
+
+  it 'is valid with a name of 60 characters or less' do
+    user.name = ('a' * 60)
+    expect(user).to be_valid
+
+    user.name = ('a' * 40)
+    expect(user).to be_valid
+  end
+
+  it 'is not valid with a name of less than characters' do
+    user.name = 'a'
+    expect(user).to_not be_valid
+  end
+
+  it 'is not valid with a password shorter than 6 characters' do
+    user.name = 'John Doe'
+    user.password = '12345'
+    user.password_confirmation = '12345'
+    expect(user).to_not be_valid
+  end
+
+  it 'is valid with a password of 6 characters or more' do
+    user.password = '1234569'
+    user.password_confirmation = '1234569'
+    expect(user).to be_valid
+  end
+
+  it 'is not valid with a password confirmation that does not match' do
+    user.password = '1234569'
+    user.password_confirmation = '1234567'
+    expect(user).to_not be_valid
+  end
+
+  it 'is valid with a password confirmation that matches' do
+    user.password = '1234569'
+    user.password_confirmation = '1234569'
+    expect(user).to be_valid
+  end
+
+  describe 'Associations' do
+    it 'should have many categories and entities' do
+      user = User.reflect_on_association(:categories)
+      expect(user.macro).to eq(:has_many)
+      user = User.reflect_on_association(:entities)
+      expect(user.macro).to eq(:has_many)
+    end
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,8 +6,8 @@ require_relative '../config/environment'
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 require_relative 'support/factory_bot'
-# Add additional requires below this line. Rails is not loaded until this point!
 
+# Add additional requires below this line. Rails is not loaded until this point!
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -30,6 +30,7 @@ begin
 rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
+
 RSpec.configure do |config|
   config.include Devise::Test::IntegrationHelpers, type: :request
   config.include Devise::Test::IntegrationHelpers, type: :feature
