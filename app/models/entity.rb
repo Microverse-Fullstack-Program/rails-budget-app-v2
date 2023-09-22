f4419bbe3f510b7f377b3ef591d186b75a9e4d1b
cherelemma <chere.lemma@aastu.edu.et>
2023-09-22 21:52:34 +0300

    add locale error message and display inline with form elment

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -3,5 +3,5 @@ class Entity < ApplicationRecord
   belongs_to :category, class_name: 'Category', foreign_key: 'category_id'
 
   validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 60 }
-  validates :amount, presence: true, numericality: { greater_than: 0 }, length: { minimum: 1, maximum: 100 }
+  validates :amount, presence: true, numericality: { greater_than: 0, less_than: 10**5 }
 end
